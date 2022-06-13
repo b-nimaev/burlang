24172bcb539d02a8ec10f47c20bebbb92972ae32
Alexandr <moyhoroshenkiy@gmail.com>
2022-06-14 01:46:39 +0800

    update

--- index.ts
+++ index.ts
@@ -1,23 +1,68 @@
-import { Telegraf } from 'telegraf'
+import { Scenes, session, Telegraf, Context } from 'telegraf'
+const localtunnel = require("localtunnel")
+const express = require("express")
+require("dotenv").config()
 
-const bot = new Telegraf("5185323202:AAHpxdNcdI0Q9NX_nEAvTxu7qC351W7V46E")
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('hipster', Telegraf.reply('λ'))
-bot.command('quit', (ctx) => {
-    // Explicit usage
-    ctx.telegram.leaveChat(ctx.message.chat.id)
+interface MySceneSession extends Scenes.SceneSessionData {
+    myScenesSessionProp: number
+}
 
-    // Using context shortcut
-    ctx.leaveChat()
+interface MySession extends Scenes.SceneSession<MySceneSession> {
+    mySessionProp: number
+}
+
+interface BContext extends Context {
+    myProp?: string,
+    session: MySession,
+    scene: Scenes.SceneContextScene<BContext, MySceneSession>
+}
+
+const { enter, leave } = Scenes.Stage
+
+const greeterScene = new Scenes.BaseScene<BContext>('greeter')
+greeterScene.enter((ctx) => ctx.reply('greeter scene'))
+greeterScene.leave((ctx) => ctx.reply('greeter scene exit'))
+greeterScene.hears("Привет", (ctx) => ctx.scene.enter('greeter'))
+greeterScene.on("message", (ctx) => ctx.replyWithMarkdown('Отправьте `Привет`'))
+
+const echoScene = new Scenes.BaseScene<BContext>("echo")
+echoScene.enter((ctx) => ctx.reply("сцена вывода"))
+echoScene.leave((ctx) => ctx.reply("выход из сцены вывода"))
+echoScene.command("back", leave<BContext>())
+echoScene.on("text", (ctx) => ctx.reply(ctx.message.text))
+echoScene.on("message", (ctx) => ctx.reply("Only text messages please"))
+
+const bot = new Telegraf<BContext>(<string>process.env.BOT_TOKEN)
+const app = express()
+
+const stage = new Scenes.Stage<BContext>([greeterScene, echoScene], {
+    ttl: 10
 })
-bot.on('text', (ctx) => {
-    // Explicit usage
-    ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-
-    // Using context shortcut
-    ctx.reply(`Hello ${ctx.state.role}`)
+bot.use(session())
+bot.use(stage.middleware())
+bot.use((ctx, next) => {
+    // @ts-ignore
+    ctx.myProp = ctx.chat?.first_name
+    return next()
 })
-bot.launch()
+
+bot.start((ctx) => {
+    ctx.reply('Привет, ' + ctx.myProp + '!')
+    console.log(ctx.myProp)
+})
+bot.command("greeter", (ctx) => ctx.scene.enter("greeter"))
+bot.command("echo", (ctx) => ctx.scene.enter("echo"))
+bot.on("message", (ctx) => ctx.reply("Try /echo or /greeter"))
+
+// Backend
+const secretPath = `${bot.secretPathComponent()}`
+
+// @ts-ignore
+localtunnel({ port: 3000 }).then((result: any) => bot.telegram.setWebhook('https://say-an.ru/' + secretPath).then(data => {
+    console.log(data)
+}))
+app.use(bot.webhookCallback('https://say-an.ru/' + secretPath))
+app.listen(3000, () => console.log("telegram bot launched!"))
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
